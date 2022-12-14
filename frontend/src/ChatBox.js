6fa50f8e4a53eb641a506b5972133f5bc8313739
Oskar Korczak <oskar.korczak@protonmail.com>
2022-12-14 23:13:17 +0100

    fix

--- frontend/src/ChatBox.js
+++ frontend/src/ChatBox.js
@@ -20,12 +20,12 @@ export default function ChatBox() {
         const API_ROOT = 'http://localhost:8000';
 
 
-// curl -X 'POST' \
-//   'http://localhost:8000/api/chat?message=xd' \
-//   -H 'accept: application/json' \
-//   -d ''
-// mode: 'no-cors',
-// method: 'POST',
+        // curl -X 'POST' \
+        //   'http://localhost:8000/api/chat?message=xd' \
+        //   -H 'accept: application/json' \
+        //   -d ''
+        // mode: 'no-cors',
+        // method: 'POST',
 
         const response = await fetch(`${API_ROOT}/api/chat?message=${message}`, {
             method: 'POST',
@@ -33,55 +33,54 @@ export default function ChatBox() {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             },
-            mode: 'no-cors',
         });
-        const data = await response.text();
+        const data = await response.json();
         console.log(data);
         setAnswer(data.response);
 
 
-       
+
     }
-  return (
-    // Display paper centered in the screen with width 500px
-    <Paper sx={{ width: 500, margin: 'auto', marginTop: 2, padding: 2 }}>
-        <h1>ChatBox</h1>
-        
+    return (
+        // Display paper centered in the screen with width 500px
+        <Paper sx={{ width: 500, margin: 'auto', marginTop: 2, padding: 2 }}>
+            <h1>ChatBox</h1>
 
-        
-        <form onSubmit={handleSubmit}>
-           
-        <TextField
-            id="outlined-multiline-static" 
-            label="Message"
-            placeholder="Type your message here"
-            multiline
-            rows={4}
-            fullWidth
-            variant="outlined"
-            onKeyDown={(event) => {
-                // ignore keypresses if the user is holding down the shift key
-                if (event.shiftKey) {
-                    return;
-                }
-                if (event.key === 'Enter') {
-                    handleSubmit(event);
-                }
-            }}
-        />
-         </form>
 
-       {/* Place for the answer, rendered conditionally  */}
 
-         {answer && (
-            <Box sx={{ bgcolor: 'grey.300', p: 2, mt: 2 }}>
-                {answer}
-            </Box>
-        )}
-       
+            <form onSubmit={handleSubmit}>
 
-       
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Message"
+                    placeholder="Type your message here"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    variant="outlined"
+                    onKeyDown={(event) => {
+                        // ignore keypresses if the user is holding down the shift key
+                        if (event.shiftKey) {
+                            return;
+                        }
+                        if (event.key === 'Enter') {
+                            handleSubmit(event);
+                        }
+                    }}
+                />
+            </form>
+
+            {/* Place for the answer, rendered conditionally  */}
+
+            {answer && (
+                <Box sx={{ bgcolor: 'grey.300', p: 2, mt: 2 }}>
+                    {answer}
+                </Box>
+            )}
+
+
+
 
         </Paper>
-  );
+    );
 }
