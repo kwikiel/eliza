555b094b953e907eafb834c6a11c6403997e9571
Kacper Wikieł <k.wikiel@gmail.com>
2022-12-20 18:19:15 +0100

    Fix

--- frontend/src/ChatBox.js
+++ frontend/src/ChatBox.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
--- local.sh
+++
@@ -1,7 +0,0 @@
-# run uvicorn in the background from the shell script with poetry run
-OPENAI_API_KEY=1337 poetry run uvicorn app:app --host 0.0.0.0 &
-FOO_PID=$!
-echo $FOO_PID 
-
-cd frontend && npm start
-kill $FOO_PID
