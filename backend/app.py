6decf65573b9b0dc2367545d57010f2f70abde7c
Kacper Wikieł <k.wikiel@gmail.com>
2022-12-20 21:05:23 +0100

    Dockerized

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,6 @@ from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 
-
-
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -23,6 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class AIAnswer(BaseModel):
     response: str
 
@@ -33,13 +31,9 @@ def root():
     return RedirectResponse(url="/docs")
 
 
-
 @app.post("/api/chat", response_model=AIAnswer)
 def chat(message: str):
     chatbot_model = ChatBot()
     response = chatbot_model.generate(message)
     print(AIAnswer(response=response))
     return AIAnswer(response=response)
-
-
-
